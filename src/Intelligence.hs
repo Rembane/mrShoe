81eb38d9dd6a623ff9de9f35613c690091037f88
Andreas Ekeroot <andeke@gmail.com>
2015-09-05 13:42:43 +0200

    Made the type signatures in Intelligence prettier.

--- src/Intelligence.hs
+++ src/Intelligence.hs
@@ -3,6 +3,8 @@ module Intelligence (createDatabase, generateLine) where
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
 import qualified Data.Vector as V
+import Data.Text (Text)
+import Data.Vector (Vector)
 import System.Random (StdGen, randomR, randomRs)
 
 -- | Algorithm gratefully borrowed from
@@ -13,7 +15,7 @@ import System.Random (StdGen, randomR, randomRs)
 -- | Maps prefixes to the a list of the next word.
 -- Also contains the length of the prefix.
 data DB = DB {
-  database     :: M.Map (V.Vector T.Text) (V.Vector T.Text),
+  database     :: M.Map (Vector Text) (Vector Text),
   prefixLength :: Int
 }
 
@@ -23,7 +25,7 @@ randomR1 g max = randomR (0, max) g
 
 -- | The order of the Markov chain, or length of the prefix, in number of words.
 -- Returns a list of prefixes and their suffixes.
-toMarkovPairs :: Int -> T.Text -> [(V.Vector T.Text, T.Text)]
+toMarkovPairs :: Int -> Text -> [(Vector Text, Text)]
 toMarkovPairs i s = zipWith combine prefixes (tail prefixes)
   where
     words       = V.fromList $ filter (not . T.null) $ T.splitOn " " s
@@ -32,7 +34,7 @@ toMarkovPairs i s = zipWith combine prefixes (tail prefixes)
     combine a b = (a, V.last b)
 
 -- | Create a database of Markov wisdom from a prefix length and a list of texts.
-createDatabase :: Int -> [T.Text] -> DB
+createDatabase :: Int -> [Text] -> DB
 createDatabase prefixLen texts = DB { database = M.fromListWith (V.++) 
                                                $ map (fmap V.singleton) 
                                                $ concatMap (toMarkovPairs prefixLen) texts,
@@ -41,32 +43,32 @@ createDatabase prefixLen texts = DB { database = M.fromListWith (V.++)
 
 
 -- | Create the first phrase of a message.
-firstPhrase :: StdGen -> DB -> (V.Vector T.Text, StdGen)
+firstPhrase :: StdGen -> DB -> (Vector Text, StdGen)
 firstPhrase g db = let (pidx,   g' ) = randomR1 g  $ (M.size   $ database db)-1
                        (sidx,   g'') = randomR1 g' $ (V.length            ss)-1
                        (prefix, ss ) = M.elemAt pidx $ database db
                     in (V.snoc prefix (ss V.! sidx), g'')
 
 -- | Generate a randomly selected suffix for a prefix, and append it to the suffix.
-nextPhrase :: V.Vector T.Text -> StdGen -> DB -> Maybe (V.Vector T.Text, StdGen)
+nextPhrase :: Vector Text -> StdGen -> DB -> (Maybe (Vector Text), StdGen)
 nextPhrase prefix g db = let len = prefixLength db
                              p'  = V.slice (((V.length prefix)-1) - len) len prefix
                           in case M.lookup p' (database db) of
-                               Nothing -> Nothing
+                               Nothing -> (Nothing, g)
                                Just ws -> let (widx, g') = (randomR1 g $ (V.length ws)-1)
-                                           in Just (V.snoc prefix (ws V.! widx), g')
+                                           in (Just $ V.snoc prefix (ws V.! widx), g')
 
 -- | Use the Markov-chain to generate a line of a certain length.
-generateLine :: Int -> StdGen -> DB -> (T.Text, StdGen)
+generateLine :: Int -> StdGen -> DB -> (Text, StdGen)
 generateLine length g db = (V.foldl1 (\t1 t2 -> t1 `T.append` (' ' `T.cons` t2)) result, g'')
   where
     (first, g')   = firstPhrase g db
     (result, g'') = generateLine' length first g' db
 
-    generateLine' :: Int -> V.Vector T.Text -> StdGen -> DB -> (V.Vector T.Text, StdGen)
+    generateLine' :: Int -> Vector Text -> StdGen -> DB -> (Vector Text, StdGen)
     generateLine' w ts g db = case nextPhrase ts g db of
-                                  Nothing        -> (ts, g)
-                                  Just (ts', g') -> if V.length ts' >= w
+                                  (Nothing, g')  -> (ts, g')
+                                  (Just ts', g') -> if V.length ts' >= w
                                                        then (ts', g')
                                                        else let (ts'', g'') = generateLine' w ts' g' db
                                                              in (ts' V.++ ts'', g'')
